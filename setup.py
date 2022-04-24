a885f0a749ed17bb5eba9e6e84ac94fb830e3e19
JustScott <>
2022-04-24 18:58:53 -0500

    Fixed synchronization error betweem github and pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open("README.md", "r") as file:
     long_description = file.read()
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = "Easy to use implementation of the built in 'socket' library"
 
 # Setting up
